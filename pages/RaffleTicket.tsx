3e8ab983e7ca101de2ff75738860a11dc5655353
LetMeP1ay <vdaberezin@gmail.com>
2024-06-26 14:25:28 +1200

    test

--- pages/RaffleTicket.tsx
+++ pages/RaffleTicket.tsx
@@ -21,20 +21,17 @@ export default function RaffleTicket() {
     await client.models.Ticket.create({});
   };
 
-  const redeemTicket = async (id: string, userId: string) => {
+  const redeemTicket = async (id: string) => {
+    tickets.filter((ticket) => {
+      return ticket.id === id;
+    });
     const updatedData = {
       id: id,
       isRedeemed: true,
-      ownerId: userId,
     };
 
     const { data: updatedTicket, errors } = await client.models.Ticket.update(
       updatedData
-    );
-    setTickets((prevTickets) =>
-      prevTickets.map((ticket) =>
-        ticket.id === id ? { ...ticket, isRedeemed: true } : ticket
-      )
     );
   };
 
@@ -45,9 +42,7 @@ export default function RaffleTicket() {
         {tickets.map(({ id, isRedeemed, owner }) => (
           <li key={id}>
             Your ticket is currently {isRedeemed ? "Redeemed" : "Not redeemed"}
-            <button onClick={() => redeemTicket(id, owner!)}>
-              Redeem your ticket
-            </button>
+            <button onClick={() => redeemTicket(id)}>Redeem your ticket</button>
           </li>
         ))}
       </ul>
